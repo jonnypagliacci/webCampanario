ac4857d3ee7048d071c69fd1c0cc22396bee29cb
jonnypagliacci <dr.sergio.valdovinos@gmail.com>
2023-02-15 11:12:16 -0600

    subo componente boton pero faalta arrelar que se vea el icono

--- src/Components/Wats.tsx
+++ src/Components/Wats.tsx
@@ -1,8 +1,7 @@
-
-export default function BotonWatss () {
+export default function BotonWatss() {
     return (
         <div>
-            <a href="https://api.whatsapp.com/send?phone=5195508107&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20Varela%202." className="float"></a>
+            <a href="https://api.whatsapp.com/send?phone=5214427157000&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20Estudios%202." className="float"></a>
             <i className="fa fa-whatsapp my-float"></i>
         </div>
     )
